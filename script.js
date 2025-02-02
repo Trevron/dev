58838637e9307e863a3ef6594b300cc7cb5c2185
Trevron <trevrontv@gmail.com>
2025-02-02 04:45:51 +0100

    Update about and comment out a bunch of stuff. Adjust CSS. Format with prettier.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for reaching out! I will get back to you soon.');
-  });
+document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (e) {
+        e.preventDefault();
+        alert(
+            "Thank you for reaching out! But this isnt actually hooked up yet haha"
+        );
+    });
