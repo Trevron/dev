88adb6e1fed82eb6ba4e4cdeb5c0ab83a0d09a44
trevron <trevrontv@gmail.com>
2026-01-22 00:23:24 +0100

    Add hyperslice to projects list

--- js/release-modal/index.js
+++ js/release-modal/index.js
@@ -1,12 +1,46 @@
-const modalButton = document.getElementById("blockshopAlbumLink");
+const blockshopButton = document.getElementById("blockshopAlbumLink");
+const blockshopPlayer = document.getElementById("blockshopPlayer");
+const hypersliceButton = document.getElementById("hypersliceAlbumLink");
+const hyperslicePlayer = document.getElementById("hyperslicePlayer");
 
 const releaseModal = document.getElementById("releaseModal");
 const closeModal = document.querySelector(".close-modal");
+const releaseTitle = document.getElementById("releaseTitle");
+const releaseLinks = document.getElementById("releaseLinks");
 
-// Initialize modal
-modalButton.addEventListener("click", () => {
+const blockshopDetails = {
+  iFrameSrc: "https://bandcamp.com/EmbeddedPlayer/album=3816339469/size=large/bgcol=333333/linkcol=0f91ff/tracklist=true/artwork=none/transparent=true/",
+  albumUrl: "https://trevron.bandcamp.com/album/block-shop-official-game-soundtrack",
+  hyperfollow: "https://distrokid.com/hyperfollow/trevron/block-shop-official-game-soundtrack",
+  title: "Block Shop (Official Game Soundtrack)",
+}
+
+const hypersliceDetails = {
+  iFrameSrc: "https://bandcamp.com/EmbeddedPlayer/album=1454649338/size=large/bgcol=333333/linkcol=0f91ff/tracklist=true/artwork=none/transparent=true/",
+  albumUrl: "https://trevron.bandcamp.com/album/hyperslice-official-game-soundtrack",
+  hyperfollow: "https://distrokid.com/hyperfollow/trevron/hyperslice-official-game-soundtrack",
+  title: "Hyperslice (Official Game Soundtrack)",
+}
+
+// Open blockshop
+blockshopButton.addEventListener("click", () => {
+  releaseTitle.innerText = blockshopDetails.title;
+  releaseLinks.href = blockshopDetails.hyperfollow;
+  blockshopPlayer.style.display = "block";
+  hyperslicePlayer.style.display = "none";
   openMusicModal();
 });
+
+// Open hyperslice
+hypersliceButton.addEventListener("click", () => {
+  releaseTitle.innerText = hypersliceDetails.title;
+  releaseLinks.href = hypersliceDetails.hyperfollow;
+  blockshopPlayer.style.display = "none";
+  hyperslicePlayer.style.display = "block";
+  openMusicModal();
+});
+
+
 
 // Close modal handlers
 closeModal.addEventListener("click", closeMusicModal);
